ddc1ad22e3e362e096ae315c77dd99b6113ffbf8
Zsuzsa Rita Bogár <alassea.indilwen@gmail.com>
2018-04-20 12:08:05 +0200

    Add test for valid signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,4 +11,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       end
       assert_template 'users/new'
     end
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Moist von Lipwig",
+                                         email: "scoundrel@postoffice.am",
+                                         password: "adorable",
+                                         password_confirmation: "adorable"}}
+      end
+      follow_redirect!
+      assert_template 'users/show'
+    end
 end
