45a7eea1fb709b108d6c3e97ea879c9c3a3dcd73
Zsuzsa Rita Bogár <alassea.indilwen@gmail.com>
2018-04-24 14:25:09 +0200

    Minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name: "Example User",
-             email: "example@gmail.com",
+User.create!(name: "Earthsong",
+             email: "earthsong@gmail.com",
              password: "python",
              password_confirmation: "python",
              admin: true,
